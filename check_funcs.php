66c8bbea9471da451fd15b1440b2021b98d22330
Phuc710 <athanhphuc7102005@gmail.com>
2026-03-07 20:41:40 +0700

    Cleanup debug files

--- check_funcs.php
+++
@@ -1,8 +0,0 @@
-<?php
-$funcs = ['exec', 'shell_exec', 'system', 'passthru', 'proc_open', 'popen'];
-$results = [];
-foreach ($funcs as $f) {
-    $results[$f] = function_exists($f) && !in_array($f, array_map('trim', explode(',', ini_get('disable_functions'))));
-}
-header('Content-Type: application/json');
-echo json_encode($results);
